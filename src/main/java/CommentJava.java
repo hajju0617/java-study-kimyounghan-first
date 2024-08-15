79f5b55b6bb143d5c6dd1d47cdd875c7f7ccb4d4
hajju0617 <joowon023617@gmail.com>
2024-08-15 23:19:44 +0900

    java 복습

---
+++ src/main/java/CommentJava.java
@@ -0,0 +1,15 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1");  // 주석주석
+//        System.out.println("Hello Java2");
+
+        /*
+
+        System.out.println("1111");
+        System.out.println("2222");
+
+         */
+
+
+    }
+}
---
+++ src/main/java/variable/ex/VarEx3.java
@@ -0,0 +1,14 @@
+package variable.ex;
+
+import java.util.Arrays;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        boolean booleanVar = true;
+
+        System.out.println(longVar);
+        System.out.println(booleanVar);
+
+    }
+}
